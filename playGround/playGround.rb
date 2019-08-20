2ce6c2d7e8f77528cd8798353fa867ea335487cf
Max Kelly <max@revelthat.com>
2019-08-20 16:22:50 +1000

    added more var and questions to playGround

--- playGround/playGround.rb
+++ playGround/playGround.rb
@@ -7,6 +7,10 @@ puts "Hi #{name}, can you tell me how old are you?"
 age = gets.chomp
 puts "That's great so everyone calls you #{name} and your age is: #{age}. Whats your favourite sport?"
 sport = gets.chomp
+puts "Cool, so your favourite sport is #{sport} and how many siblings do you have?"
+siblings = gets.chomp
+puts "Okay here is what we know about you #{name}: Your favourite sport is #{sport} and you played that with your #{siblings} siblings"
+
 
 
 
