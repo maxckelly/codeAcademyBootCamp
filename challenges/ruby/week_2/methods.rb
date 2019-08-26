628a7505cb1fb355acbeba257deec4aa0983f497
Max Kelly <max@revelthat.com>
2019-08-26 14:58:49 +1000

    Added Day 5, started methods challenge

---
+++ challenges/ruby/week_2/methods.rb
@@ -0,0 +1,296 @@
+# Working with methods
+
+# 1. Define a method that takes a string as an argument, and then puts this string to screen. Call this argument three times, once with your name, and two more times with the names of two classmates.
+
+    # def name(aString)
+    #     return aString
+    # end
+
+    # name1 = "Max"
+    # name2 = "John"
+    # name3 = "Alex"
+
+    # p name("Max")
+    # p name(name2)
+    # p name(name3)
+
+# 2. Define a method that takes two arguments and makes a hash out of them; the first argument being the first value and the second being the second value. You can choose what to call your keys. Return this hash from the method. Now call the method three times with different values each time. Store the result of these method calls in variables, and check that these variables contain hashes.
+
+    # def method_with_two_arguments(argu1, argu2)
+    #    return users = {
+    #         name: argu1,
+    #         age: argu2
+    #     }
+    # end
+
+    # p method_with_two_arguments("Max", 21)
+
+
+# 3. Define a method that takes in a number, and tells you whether it is odd or even. Call the method with 3 odd numbers and 3 even numbers.
+
+    # def odd_even(num1)
+    #     if (num1.even?)
+    #         return "Even"
+    #     else ()
+    #         return "False"
+    #     end
+    # end
+
+    # p odd_even(2)
+
+# 4. Define a method that takes in no arguments and prints/puts to screen the date today
+
+    # def date()
+    #     puts Time.now
+    # end
+   
+    # date()
+
+# 5. Calculate the area of the following shapes: rectangle, triangle, circle.
+
+    # def circle(radius)
+    #     return π * radius
+    # end
+
+    # p circle(190)
+
+    # def rectangle(length, width)
+    #     return length * width
+    # end
+
+    # p rectangle(19, 30)
+
+    # def triangle(base, height)
+    #     return height * base / 2
+    # end
+
+    # p triangle(19, 30)
+   
+# 6. Create a temperature converter that converts the temperature from celsius to fahrenheit.
+
+    # def converter_to_fahrenheit(temp)
+    #     return temp * 9/5 + 32
+    # end
+
+    # p converter_to_fahrenheit(20)
+   
+# 7. Implement methods that perform all of the basic arithmetic functions.
+
+    # def basic_arithmetic(num1, num2, operator)
+
+    #     if (operator == "+")
+    #         return num1 + num2
+    #     elsif (operator == "-")
+    #         return num1 - num2
+    #     elsif (operator == "*")
+    #         return num1 * num2
+    #     elsif (operator == "/")
+    #         return num1 / num2
+    #     else 
+    #         return "Enter an argument"
+    #     end
+    # end 
+
+    # p basic_arithmetic(30, 10, "/")
+   
+# 8. Write function that translates a text to Pig Latin and back.
+#    - English is translated to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding 'ay'.
+#    - "The quick brown fox" becomes "Hetay uickqay rownbay oxfay".
+
+    def translate(phrase)
+        phrase.reverse + "ay"
+    end
+
+    puts "what phrase would you like to make pig latin"
+    print "> "
+    phrase = gets.chomp
+
+    p translate(phrase)
+
+# 9. Implement a function that takes two numbers and an operator, and that uses your methods to perform the relevant operation
+
+    # def basic_arithmetic(num1, num2, operator)
+
+    #     if (operator == "+")
+    #         return num1 + num2
+    #     elsif (operator == "-")
+    #         return num1 - num2
+    #     elsif (operator == "*")
+    #         return num1 * num2
+    #     elsif (operator == "/")
+    #         return num1 / num2
+    #     else 
+    #         return "Enter an argument"
+    #     end
+    # end 
+   
+# 10. Implement your own versions of: .select, .insert, .delete, .pop, .shift
+
+# Case statements
+# 11. You're running a marathon and you want to make an app for a watch to give you encouraging messages depending on certain distances you've covered.
+# a. Ask the user to for input "how far have you run so far?" and store the value using gets
+# b. Define a case statement for the distance and puts the following messages, when 10 puts "long way to go but you're doing amazing!", when 20 puts "almost halfway now, killing it!", when 30 puts "now for the final push, keep going!" and when 40 puts "sprint!!!!"
+
+    # def marathon()
+
+    #     puts "How far have you run so far?"
+    #     print "> "
+    #     user_input = gets.chomp.to_f
+    
+    #     distance = user_input
+
+    #     if (distance <= 10)
+    #         return "Long way to go"
+    #     elsif (distance >= 10 &&  distance <= 20)
+    #         return "Almost halfway now, killing it!"
+    #     elsif (distance >= 20 && distance <= 30)
+    #         return "Now for the final push"
+    #     elsif (distance >= 30 && distance <= 40)
+    #         return "Sprint!!!"
+    #     else 
+    #         return "Are you running?"
+    #     end
+    # end
+
+    # p marathon()
+
+# 12. Create a petrol calculator
+
+# a. Ask the user how much petrol they have in the tank currently, and what their total petrol capacity is using a gets
+# b. Create a percentage_capacity (out of 100) for how much fuel is currently in the tank in percentage
+# c. Using a case statement when the percentage_capacity is between 1 and 25 puts "lucky you didn't run out", when the percentage_capacity is between 26 and 50 puts "fill up when the petrol price is cheap", when the percentage_capacity is between 51 and 75 puts "you already have a fair bit of fuel in the tank", when the percentage_capacity is between 76 and 100 puts "your tank is basically full!" else puts "your tank is empty" 
+
+    # def fuel_tank()
+
+    #     p "What is your total petrol capacity?"
+    #     print "> "
+    #     total_capacity = gets.chomp.to_f
+
+    #     p "How much fuel do you have in your tank?"
+    #     print "> "
+    #     current_tank = gets.chomp.to_i
+
+    #     percentage = (current_tank / total_capacity) * 100
+
+    #     case percentage
+    #     when 0 
+    #         return "You've run out of gas"
+    #     when 1..25
+    #         return "Fill up when you see a petrol station"
+    #     when 26..50
+    #         return "Fill up when the petrol price is low"
+    #     when 51..75
+    #         return "You already have a fair bit of fuel in the tank"
+    #     when 76..100
+    #         return "Your tank is basically full"
+    #     when 100
+    #         return "Your tank is full"
+    #     end
+    # end
+
+    # p fuel_tank()
+
+# Case statements and methods
+
+# 13. Grade calculator
+
+# a. Define an array of hashes that stores 3 student marks, for example:
+# b. Define a method named add_student_mark, that when called takes the students array as an argument, the method asks for user input about the name of the student and the mark of the student
+# c. When the mark of the student is stored define another method called get_grade, this method when called takes the mark as an argument
+# d. Inside of the get_grade method write a case statement, this case statement uses the mark as its case, it uses the Australian Grading System https://www.scholaro.com/pro/countries/Australia/Grading-System to return either an HD, D, C, P or N depending on what the mark is
+# e. Store the return value from the get_grade method call in a variable called grade, then create a hash that includes the name, mark and grade of the student before pushing this hash into the students array
+# f. Now the students array contains the new students define another method called display_students that takes the students array as an argument when called, the display students method can return nil but it needs to print to terminal the students in the following format:
+
+    # students = [
+    #     {
+    #         name: "Harrison Malone",
+    #         mark: 76,
+    #         grade: "D"
+    #     },
+    #     {
+    #         name: "Aaron Mendosa",
+    #         mark: 96,
+    #         grade: "HD"
+    #     },
+    #     {        
+    #         name: "Harrison Malone",
+    #         mark: 76,
+    #         grade: "D"
+    #     },
+    #     {
+    #         name: "Aaron Mendosa",
+    #         mark: 96,
+    #         grade: "HD"
+    #     }
+    # ]
+
+    # def add_student_mark(students)
+
+    #     puts "What is the students name?"
+    #     print "> "
+    #     student_name = gets.chomp
+        
+    #     puts "What was the students mark?"
+    #     print "> "
+    #     student_mark = gets.chomp
+
+    #     def get_grade(student_mark)
+        
+    #         case student_mark 
+    #             when 0
+    #                 return "F"
+    #             when 1..24.99
+    #                 return "E"
+    #             when 25..49.99
+    #                 return "D"
+    #             when 50..69.99
+    #                 return "C"
+    #             when 70..84.99
+    #                 return "B"
+    #             when 85..100
+    #                 return "A"
+    #         end
+    #     end
+
+    #     student = {
+    #         name: student_name,
+    #         mark: student_mark,
+    #         grade: get_grade(student_mark)
+    #     }
+
+    #     students.push(student)
+    # end
+
+    # p add_student_mark(students)
+
+# Extra challenges for methods
+
+# 14. In this challenge, you need to write a method prime? that takes an argument and returns true or false depending on if the number is prime or not.
+
+# You can test your answers here https://www.hackerrank.com/challenges/ruby-methods-introduction/
+
+# 15. In this challenge, your task is to figure out what take method does using the examples below and implement the method. It should help you understand how to build on implementation through the expected functionality.
+
+# > take([1,2,3], 1)
+# [2, 3]
+
+# > take([1,2,3], 2)
+# [3]
+
+# > take([1,2,3])
+# [2, 3]
+
+# You can test your answers here https://www.hackerrank.com/challenges/ruby-methods-arguments/
+
+# 16. Write a method named full_name that generates the full names of people given their first name, followed by some variable number of middle names, followed by their last name.
+
+# > full_name('Harsha', 'Bhogle')
+# "Harsha Bhogle"
+
+# > full_name('Pradeep', 'Suresh', 'Satyanarayana')
+# "Pradeep Suresh Satayanarayana"
+
+# > full_name('Horc', 'G.', 'M.', 'Moon')
+# "Horc G. M. Moon"
+
+# You can test your answers here https://www.hackerrank.com/challenges/ruby-methods-variable-arguments/
---
+++ week-two/bootCamp-Day-5.rb
@@ -0,0 +1,85 @@
+# -- Basic Info of Methods --
+    # Methods are a sequents of commands that can be used in a program over and over again
+    # Methods helps keep your code DRY - Don't Repeat Yourself 
+    # Methods always returns something
+    # A method always starts with def
+    # To call a method you do <method_name>() 
+    # Always return in a method. If you have a puts though in the method it will return nil at the end
+    # Once a method see the return line it stops completely
+
+# -- Some example --
+    #     def greet
+    #         return "Hello"
+    #     end
+
+    #     # To call a method you do:
+    #     greet()
+    #     p greet()
+
+    #     def print_menu 
+    #         puts "
+    #         press 1 for deposit 
+    #         press 2 for deposit 
+    #         press 3 for deposit
+    #         "
+    #     end
+
+    #     print_menu
+
+# -- Methods with arguments --
+    # def addition (a, b)
+    #     a + b
+    # end
+
+    # a way to display it would be: 
+    #     p addition(2, 3)
+         # or 
+
+    #     result = addition(3, 2)
+    #     puts result
+
+    # def division (num1, num2)
+    #     num1 / num2
+    # end
+
+    # p division(4, 2)
+
+# -- Scope --
+    # Example - The below code wouldn't work because the num isn't in the scope
+        # Num is in the global scope
+        # The only way to pass this num varible into the method scope is through the arguments
+        # The my_method has its own scope called the method scope
+
+        # num = 100
+
+        #def my_method
+            # method scope
+            # return num
+        #end
+
+        # my_method()
+
+        # An example of how you would get num into scope is:
+        # num = 100 
+
+        # def my_method(num) # the parameter is a placeholder for the argument we pass. So it doesn't have to be the same name. It has no value just represents our argument 
+            # return num # Whatever the parameter is is the name you're calling. For example if (num) was called (number100) we would have to change num in method scope
+        # end
+
+        # number = my_method(num) 
+        # p number # This is now accessible in the Global scope
+
+# Calling a method inside a method
+    # Example - Calling a method inside a method 
+        number = my_method(number)
+
+        def adder(num1, num2)
+            return num1 + num2
+        end
+
+        def display_sum()
+            sum = adder(1, 5)
+            return sum
+        end
+
+        p display_sum()
