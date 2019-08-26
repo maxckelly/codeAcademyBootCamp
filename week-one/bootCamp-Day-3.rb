1aeb292b68c1669920f5bc0d702ea04add2e256e
Max Kelly <max@revelthat.com>
2019-08-27 09:10:10 +1000

    Cleaned up playGround.rb

--- week-one/bootCamp-Day-3.rb
+++ week-one/bootCamp-Day-3.rb
@@ -58,7 +58,7 @@
     # Until loop 
         # p "Enter a number"
         # x = gets.chomp.to_i
-        # while x < 0
+        # until x < 0
         #     puts x
         #     x = x - 1
         # end
