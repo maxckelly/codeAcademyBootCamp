fa58cd252119568d4b5009f84971be11a436bc0e
Max Kelly <max@revelthat.com>
2019-08-30 12:11:52 +1000

    Changed file names

---
+++ challenges/ruby/week_2/14-paper-scissors-rock.rb
@@ -0,0 +1,13 @@
+# Specs
+
+# Use a method (or class if you're feeling like a bit of an expert), to create a scissors-paper-rock (SPR) game. 
+
+# 1. 
+
+# a. The method (or class) will prompt the user for their selection ("scissors", "paper", "rock"), then will randomly select the computers choice, and will print out the result based on the rules of SPR.
+
+# Example
+
+# The user selects "scissors", random computer selection spits out "rock", print "Computer wins with rock!"
+
+# b. After the game, ask the user if they would like to play again. If they select "No" or "N", exit the method (or class), else replay the method (or class) using recursion.
