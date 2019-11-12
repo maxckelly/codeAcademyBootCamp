edaba07bd4e146d98bd6473e64c8dbddaaa9a374
Max Kelly <max@revelthat.com>
2019-11-12 16:40:13 +1100

    changes

---
+++ challenges/javaScript/week-01/01_challenge.js
@@ -0,0 +1,36 @@
+// Challenge
+// 1. Create a new variable called user, set the value to: tom
+// 2. Write an if statement: if the user's name is tom, say 'Hello Tom'
+// 3. Add an else condition: say 'Hello someone other than Tom'
+// 4. Add a new variable called age, set the value to: 18
+// 5. If the user is 18, say 'welcome to the casino!'
+
+let user = "Tom";
+let age = 22;
+
+if (age > 21) {
+  console.log("You're a adult");
+} else if (age < 21 && age >= 13 ) {
+  console.log("You are a teenager");
+} else {
+  console.log("You're a child");
+}
+
+
+// 6. Research 'prompt'.Use it in your code to ask the user for their age.
+// 7. If the user is less than 18 years old, use the alert method to deny entry.
+
+// # Beast Mode
+// 1. Assign an empty array to a students variable.
+// 2. Use the push method to add a student called Sarah
+// 3. Use prompt to ask the user for their name, add it to the array.
+// 4. Use a loop to ask the user for 3 names, add them all to the array.
+// 5. Loop through the array and use the alert method to read each user back to the user.
+
+// # Beast Mode++
+// 1. Store student objects in your students array.Each student object should have a name and age property.
+//   Collapse
+
+
+
+
---
+++ challenges/javaScript/week-01/02_challenge.js
@@ -0,0 +1,120 @@
+// 1. Make a function that takes two arguments, and returns the result of them added together.
+// 2. Call the above function four times with different inputs each time. Make at least some of the inputs floats. 
+// 3. Define two integer and two float variables, and use them as arguments to this method you have made.
+
+let adder = ( numberOne, numberTwo ) => {
+  return numberOne + numberTwo;
+};
+
+let intOne = 10
+let intTwo = 10
+let floatOne = 2.0
+let floatTwo = 2.1
+
+console.log(adder(10, 10));
+console.log(adder(30, 400));
+console.log(adder(intOne, floatOne));
+console.log(adder(intTwo, floatTwo));
+
+// ============================================
+// 4. Make an object (in ruby this would be a hash) with four keys, and have the keys be three different data types.
+// 5. Access two of the values in your object, and log them to the screen.
+
+objectOne = {
+  name: "Max",
+  age: 21,
+  livingWithParents: false,
+  location: {
+    city: "Melbourne",
+    suburb: "Hawthorn",
+    postcode: 3122
+  }
+};
+
+if ( objectOne.livingWithParents == true ) {
+  console.log(`Hi, my name is ${objectOne.name}, I am ${objectOne.age} years old and living with my parents in ${objectOne.location.city}`);
+} else if ( objectOne.livingWithParents == false ) {
+  console.log(`Hi, my name is ${objectOne.name}, I am ${objectOne.age} years old and living alone in ${objectOne.location.suburb}`);
+};
+
+// ============================================
+// 6. Create another object with one key and value pair.
+// 7. Define a function that takes that type of object as the input, and log out the value of that key.
+
+objectTwo = {
+  name: "John Smith"
+}
+
+let objectTwoFunction = ( object ) => {
+  for ( let valueKey in object ) {
+    console.log(`${valueKey}: ${object[valueKey]}`);
+  };
+};
+
+objectTwoFunction(objectTwo);
+// ============================================
+// 8. Make an object with three keys and values, and assign it to a variable.
+// 9. Make four more objects with the same keys.
+// 10. Access a value from three of these objects.
+
+objectOne = {
+  name: "G",
+  description: "Greatest ",
+  fact: true
+}
+
+objectTwo = {
+  name: "O",
+  description: "of ",
+  fact: true
+}
+
+objectThree = {
+  name: "A",
+  description: "all ",
+  fact: false
+}
+
+objectFour = {
+  name: "T",
+  description: "time",
+  fact: false
+}
+// console.log(objectOne.name);
+
+// ============================================
+// 11. Define a variable and assign to it an empty array.
+// 12. Define a function that takes an array and an object as arguments. The function will push the object into the given array.
+// 13. Using the function, the four objects, and the array that you have made in the previous steps, invoke the function four times to put those four objects into the array.
+// 14. Make sure that the array contains four hashes (maybe log to the screen).
+// 15. Access two elements of the array that you have created (via an index)
+// 16. For one of these, now access one of the values of the object at that is located at that index.
+
+let emptyArr = []
+
+let pushFunction = ( arr, ...obj ) => {
+  arr.push(...obj);
+};
+
+pushFunction(emptyArr, objectOne, objectTwo, objectThree, objectFour);
+console.log(emptyArr);
+
+emptyArr.forEach( function (value, index ) {
+  console.log(`${value.name} is at index ${index}`)
+});
+
+// ============================================
+// 17. Write a function that takes two arguments - one an object, and the other an array. Inside the function log out a value from the object, and an element from the array.
+
+
+let someFunction = ( obj, arr ) => {
+  console.log( `${obj.name}: ${arr[0].description}`)
+};
+
+someFunction( objectOne, emptyArr );
+// 18. Define four objects, one with one key, one with two keys, one with three keys, and one with four. Make the first key consistent across these objects.
+// 19. Define two different arrays, with two different datatypes.
+// 20. Call the function four times, once with each object, and use the arrays you made.
+// ============================================
+// 21. Write a function that takes five arguments: three strings, and two numbers.
+// 22. Call that function three times.
